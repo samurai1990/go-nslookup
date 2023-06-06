10fab71b2f4e5ea61337d7d8d7a6d340ead24b20
samurai <samurai@avan.local>
2023-06-06 11:10:28 +0200

    change: format style

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 
 func main() {
 
-	if len(os.Args) <= 1{
+	if len(os.Args) <= 1 {
 		log.Fatalln("Please enter argument.")
 	}
 
@@ -28,13 +28,13 @@ func main() {
 		"--headless",
 	}})
 
-	driver, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub",9515))
+	driver, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub", 9515))
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer driver.Quit()
 
-	driver.Get(fmt.Sprintf("https://www.nslookup.io/domains/%s/dns-records/",os.Args[1]))
+	driver.Get(fmt.Sprintf("https://www.nslookup.io/domains/%s/dns-records/", os.Args[1]))
 
 	links, err := driver.FindElements(selenium.ByClassName, "py-1")
 	if err != nil {
@@ -45,7 +45,7 @@ func main() {
 		if ip, err := link.Text(); err == nil {
 			parsedIP := net.ParseIP(ip)
 			if parsedIP.To4() != nil {
-				fmt.Printf("A records %s : %s \n", os.Args[1],ip)
+				fmt.Printf("A records %s : %s \n", os.Args[1], ip)
 				break
 			}
 		}
